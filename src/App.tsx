8385b91ba010746976eb69fd120b2ff9e98b9cc6
Pavel Martynov <osoko007@gmail.com>
2025-04-14 12:32:42 +0300

    first commit
    add github actions to deploy
    
    test github actions
    
    new naming github actions
    
    commit
    
    change deploy.yml
    
    build to dist deploy.yml
    
    remove  publish_branch: gh-pages
    
    test
    
    test
    
    s
    
    sss
    
    tt
    
    test
    
    basename router
    
    a
    
    f
    
    s
    
    ttt
    
    tttt
    
    awt

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <NavLink to="/any">Ссылка </NavLink>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,13 +18,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit  <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,29 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import {
   createBrowserRouter,
+  NavLink,
   RouterProvider,
 } from "react-router-dom";
 import App from './App.tsx';
 import './index.css';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/app", // обязательно начни с /
+      element: <App />,
+    },
+    {
+      path: "/any", // обязательно начни с /
+      element: <div>
+        <NavLink to="/app">Главная</NavLink>
+      </div>,
+    },
+  ],
   {
-    path: "/",
-    element: <App/>,
-  },
-]);
+    basename: "/my-site", // 👈 имя репозитория
+  }
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
